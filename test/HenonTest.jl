961f145e091e2f9fdad1a3be467f755664eefb5d
Brendan Harris <bhar9988@uni.sydney.edu.au>
2021-02-18 15:06:28 +1100

    Fix out-of-place Henon test and wrap on AR(1) test

--- test/HenonTest.jl
+++ test/HenonTest.jl
@@ -3,21 +3,19 @@ using Plots
 
 # Simulate an discrete dynamical system (Henon map) using DynamicalSystems.jl
 
-@inline @inbounds function hiip(dx, x, p, n)
-    dx[1] = 1.0 - p[1]*x[1]^2 + x[2]
-    dx[2] = p[2]*x[1]
-    return dx
+@inline @inbounds function hoop(x, p, n)
+    dx1 = 1.0 - p[1]*x[1]^2 + x[2]
+    dx2 = p[2]*x[1]
+    return SVector{2}(dx1, dx2)
 end
 # Jacobian:
-@inline @inbounds function hiip_jac(J, x, p, n)
-    J[1,1] = -2*p[1]*x[1]
-    J[1,2] = 1.0
-    J[2,1] = p[2]
-    J[2,2] = 0.0
+@inline @inbounds function hoop_jac(x, p, n)
+    J = @SMatrix [-2*p[1]*x[1]      1.0;
+                p[2]            0.0]
     return J
 end
 
-ds = DiscreteDynamicalSystem(hiip, [0.0; 0.0], [1.4, 0.3], hiip_jac)
+ds = DiscreteDynamicalSystem(hoop, [0.0; 0.0], [1.4, 0.3], hoop_jac)
 
 data = trajectory(ds, 10000; dt=1)
 
