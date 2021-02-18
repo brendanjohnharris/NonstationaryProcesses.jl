4bf9fb9248037523c69dd107b7f0341661d3d8e0
Brendan Harris <bhar9988@uni.sydney.edu.au>
2021-02-18 15:06:28 +1100

    Fix out-of-place Henon test and wrap on AR(1) test

--- test/ARTests.jl
+++ test/ARTests.jl
@@ -1,24 +1,63 @@
 using DynamicalSystems
 using Plots
+using Random
 
 # Simulate an AR process using DynamicalSystems.jl
 
-@inline @inbounds function hiip(dx, x, p, n)
-    dx[1] = 1.0 - p[1]*x[1]^2 + x[2]
-    dx[2] = p[2]*x[1]
-    return dx
+@inline @inbounds function AR(X, p, t)
+    r = randn()
+    println(r)
+    X = r + (p).*X
+    println(X)
+    return X
 end
-# Jacobian:
-@inline @inbounds function hiip_jac(J, x, p, n)
-    J[1,1] = -2*p[1]*x[1]
-    J[1,2] = 1.0
-    J[2,1] = p[2]
-    J[2,2] = 0.0
-    return J
+Random.seed!(32)
+ds = DiscreteDynamicalSystem(AR, 0.0, 0.1)
+
+# ----------- Look more closely at the integrator-----------
+# function cut_step!(integ, N)
+#     #println(integ.u[1])
+#     for i in 1:N
+#         integ.u = integ.f(integ.u, integ.p, integ.t)
+#         integ.t += 1
+#     end
+#     return
+# end
+
+# function cut_trajectory(ds, t, u = ds.u0; dt = 1, Ttr = 0) where {S}
+#     dt = round(Int, dt)
+#     ti = ds.t0
+#     tvec = ti:dt:t+ti
+#     L = length(tvec)
+#     integ = integrator(ds, u)
+#     Ttr != 0 && step!(integ, Ttr)
+#     data = zeros(L)
+#     data[1] = integ.u[1]
+#     for i in 2:L
+#         #print(integ)
+#         cut_step!(integ, dt)
+#         data[i] = integ.u[1]
+#     end
+#     return data
+# end
+#data = cut_trajectory(ds, 25; dt=1)
+#----------------------------------------
+data = trajectory(ds, 25; dt=1)
+
+plot(data[:, 1], seriestype=:line)
+
+# Check that this EOM is in the correct form by reproducing:
+f(X, p) = randn() + p.*X
+function integrateAR(X0, p, T)
+    X = zeros(T+1)
+    X[1] = X0
+    for t = 1:T
+        X[t+1] = f(X[t], p)
+    end
+    return X
 end
+Random.seed!(32)
+t = integrateAR(0.0, 0.1, 25)
+plot!(t, seriestype=:line)
 
-ds = DiscreteDynamicalSystem(hiip, [0.0; 0.0], [1.4, 0.3], hiip_jac)
 
-data = trajectory(ds, 10000; dt=1)
-
-plot(data[:, 1], data[:, 2], seriestype=:scatter, markersize=0.5)
