31de9d59c01b552a481f6d2df7822c29c3a035e4
Brendan Harris <bhar9988@uni.sydney.edu.au>
2021-02-19 16:35:39 +1100

    Test time-varying parameters with discontinuities for the Van der Pol oscillator

--- src/ParameterProfiles.jl
+++ src/ParameterProfiles.jl
@@ -14,12 +14,12 @@ function heaviside(x::Real, stepOpt::Real=1.0)
     else
         return nothing
     end
-    y = convert(typeof(y), x)
+    y = convert(typeof(x), y)
 end
 heaviside(x::AbstractArray, stepOpt::Real=1.0) = map.(x -> heaviside(x, stepOpt), x)
 
 """
-    unitStep(;threshold::Real=0.0 stepOpt::Real=1.0)
+    unitStep(threshold::Real=0.0 stepOpt::Real=1.0)
 
 Construct a constant function of 'x' that undergoes a unit step at 'threshold' (the Heaviside step function translated in x by 'threshold'). 'stepOpt' sets the value at 0. A Discontinuous type is returned, which can be called like a normal function but also contains the x-value for which there is a discontinuity.
 
