4d6e21ef9a47a56949b16bc8a799ca5047a7e80e
Brendan Harris <bhar9988@uni.sydney.edu.au>
2021-02-19 15:18:44 +1100

    Add the Discontinuous type and a unitStep function

--- src/ParameterProfiles.jl
+++ src/ParameterProfiles.jl
@@ -1,17 +1,37 @@
-
+include("Discontinuous.jl")
 
 # ------------------------------------------------------------------------------------------------ #
 #                           Functions for constructing parameter profiles                          #
 # ------------------------------------------------------------------------------------------------ #
 
-function heaviside(x::Real, zeroval::Real=1.0)
+function heaviside(x::Real, stepOpt::Real=1.0)
     if x < 0
         y = 0
     elseif x > 0
         y = 1
+    elseif x == 0
+        y = stepOpt
     else
-        y = convert(typeof(x), zeroval)
+        return nothing
     end
+    y = convert(typeof(y), x)
+end
+heaviside(x::AbstractArray, stepOpt::Real=1.0) = map.(x -> heaviside(x, stepOpt), x)
+
+"""
+    unitStep(;threshold::Real=0.0 stepOpt::Real=1.0)
+
+Construct a constant function of 'x' that undergoes a unit step at 'threshold' (the Heaviside step function translated in x by 'threshold'). 'stepOpt' sets the value at 0. A Discontinuous type is returned, which can be called like a normal function but also contains the x-value for which there is a discontinuity.
+
+# Examples
+```julia-repl
+julia> D = unitStep(0.0, 1.0);
+D([-1, 0, 1])
+```
+"""
+
+function unitStep(threshold::Real=0.0, stepOpt::Real=1.0)
+    Discontinuous(x -> heaviside(x-threshold, stepOpt), Set([threshold]))
 end
 
 
