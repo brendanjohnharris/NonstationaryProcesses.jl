567568a5bf62440373422e0ac26b06a857e116b7
BrendanJHarris <brendanjohnharris@gmail.com>
2021-04-04 15:09:09 +1000

    Nice default double pendulum
    Former-commit-id: ca669a9b8555878930221d4c8d5bc19e8c70a86b

--- src/Simulations.jl
+++ src/Simulations.jl
@@ -101,29 +101,44 @@ export doublePendulumSim
 
 cartesianDoublePendulumSim = Process(
     process = cartesianDoublePendulum,
-    X0 = [3π/4, 3π/4, 0.0, 0.0], # Two angles and two momenta
+    X0 = [π/2, π/2, 0.0, 0.0], # Two angles and two momenta
     parameter_profile = (ramp, constantParameter, constantParameter, constantParameter),
-    parameter_profile_parameters = ((0.0, 1.0, 0.0), (1.0,), (1.0,), (1.0,)), # (threshold, baseline, stepHeight)
+    parameter_profile_parameters = ((-0.015, 1.0, 0.0), (1.0,), (1.0,), (2.0,)), # (threshold, baseline, stepHeight)
     transient_t0 = -10.0,
     t0 = 0.0,
-    dt = 0.01,
-    savedt = 0.1,
-    tmax = 5000.0,
+    dt = 0.00001,
+    savedt = 0.01,
+    tmax = 50.0,
     alg = AutoVern7(Rodas5()),
-    solver_opts = Dict(:adaptive => true, :reltol => 1e-8))
+    solver_opts = Dict(:adaptive => true, :reltol => 1e-20))
 export cartesianDoublePendulumSim
 
 
-waveDrivenHarmonicSim = Process(# parameters:  (ω, ε, k, Ω)
-    process = waveDrivenHarmonic,
-    X0 = [0.0, 0.0],
-    parameter_profile = (constantParameter, constantParameter, constantParameter, constantParameter),
-    # chaos: parameter_profile_parameters = ((1π,), (1.5π,), (20.0π,), (5π,)),
-    parameter_profile_parameters = ((1π,), (10.0,), (1.0π,), (0.9π)),
+cartesianDoublePendulumArt = Process(
+    process = cartesianDoublePendulum,
+    X0 = [5π/6, π, 0.0, 0.0], # Two angles (from the downward direction) and two momenta
+    parameter_profile = (lorentzian, lorentzian, constantParameter, constantParameter),
     transient_t0 = -10.0,
     t0 = 0.0,
     dt = 0.0001,
     savedt = 0.001,
+    tmax = 50.0,
+    parameter_profile_parameters = ((-1.0, 10.0, 25.0, 1.5), (-1.0, 5.0, 25.0, 2.0), (1.0,), (2.0,)),
+    alg = AutoVern7(Rodas5()),
+    solver_opts = Dict(:adaptive => false))
+export cartesianDoublePendulumArt
+
+
+waveDrivenHarmonicSim = Process(# parameters:  (ω, ε, k, Ω)
+    process = waveDrivenHarmonic,
+    X0 = [0.1, 0.1],
+    parameter_profile = (constantParameter, constantParameter, constantParameter, constantParameter),
+    parameter_profile_parameters = ((1π,), (5π,), (1.0π,), (3π,)),
+    #parameter_profile_parameters = ((1π,), (10.0,), (1.0π,), (0.9π)),
+    transient_t0 = -10.0,
+    t0 = 0.0,
+    dt = 0.0001,
+    savedt = 0.01,
     tmax = 100.0,
     alg = RK4())
 export waveDrivenHarmonicSim
@@ -143,18 +158,18 @@ pulseDrivenHarmonicSim = Process(# parameters:  (ω, ε, k, Ω)
 export pulseDrivenHarmonicSim
 
 
-skewedHarmonicSim = Process(
-    process = skewedHarmonic,
-    X0 = [1.0, 0.0],
-    parameter_profile = (constantParameter, constantParameter),
-    parameter_profile_parameters = ((1.0π,), (10.0,)), # (threshold, baseline, stepHeight)
-    transient_t0 = -10.0,
-    t0 = 0.0,
-    dt = 0.0001,
-    savedt = 0.001,
-    tmax = 100.0,
-    alg = RK4())
-export skewedHarmonicSim
+# skewedHarmonicSim = Process(
+#     process = skewedHarmonic,
+#     X0 = [1.0, 0.0],
+#     parameter_profile = (constantParameter, constantParameter),
+#     parameter_profile_parameters = ((1.0π,), (10.0,)), # (threshold, baseline, stepHeight)
+#     transient_t0 = -10.0,
+#     t0 = 0.0,
+#     dt = 0.0001,
+#     savedt = 0.001,
+#     tmax = 100.0,
+#     alg = RK4())
+# export skewedHarmonicSim
 
 
 
@@ -170,3 +185,14 @@ skewedGaussianQuadraticSim = Process(
     tmax = 1000.0,
     alg = EM())
 export skewedGaussianQuadraticSim
+
+
+gaussianBimodalSim = Process(
+    process = gaussianBimodal,
+    parameter_profile = (constantParameter, constantParameter, constantParameter),
+    parameter_profile_parameters = ((5.0,), (1.0,), (0.5)),
+    transient_t0 = 0.0,
+    t0 = 0.0,
+    savedt = 1,
+    tmax = 10000.0)
+export gaussianBimodalSim
