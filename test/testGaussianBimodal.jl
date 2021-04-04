18f3a2c40af661c558566ebbf12a828e5f635fc4
BrendanJHarris <brendanjohnharris@gmail.com>
2021-04-04 15:10:11 +1000

    Gaussian bimodal process
    Former-commit-id: 6bab87af277a23d4a5042964a2a57bf46e33e5b1

---
+++ test/testGaussianBimodal.jl
@@ -0,0 +1,19 @@
+using NonstationaryProcesses
+using Plots
+
+sim = gaussianBimodalSim(
+    parameter_profile = (constantParameter, constantParameter, constantParameter),
+    parameter_profile_parameters = ((5.0,), (1.0,), (0.5)),  # (μ, σ, α)
+    transient_t0 = 0.0,
+    t0 = 0.0,
+    savedt = 1,
+    tmax = 1000.0,
+    solver_rng=rand(UInt))
+
+S = simulate(sim)
+
+p1 = tsdensity(S, densityoffset=-15)
+
+
+S = simulate(sim(parameter_profile_parameters=((5.0,), (1.0,), (0.2))))
+p2 = tsdensity(S, densityoffset=-15)
