390f3aed5524aaa14597152cbbe139354f8885d9
BrendanJHarris <brendanjohnharris@gmail.com>
2021-05-12 11:26:23 +1000

    Add custom high dim system

--- src/Processes/Stochastic.jl
+++ src/Processes/Stochastic.jl
@@ -1,3 +1,4 @@
+using StatsBase
 # ------------------------------------------------------------------------------------------------ #
 #                                   Skewed Pitchfork Bifurcation                                   #
 # ------------------------------------------------------------------------------------------------ #
@@ -32,3 +33,31 @@ skewedGaussianQuadraticSim = Process(
     alg = EM())
 export skewedGaussianQuadraticSim
 
+
+# ------------------------------------------------------------------------------------------------ #
+#                          A system combining multiple classes of features                         #
+# ------------------------------------------------------------------------------------------------ #
+# Combine Chen's system with gaussian noise and an exponential measurement function
+function seeIfItSticks(P::Process)
+    seed(P.solver_rng)
+    prob = ODEProblem(chensSystem, P.X0, (P.transient_t0, P.tmax), tuplef2ftuple(P.parameter_profile[1:3], P.parameter_profile_parameters[1:3]), jac=chensSystem_J)
+    sol = dsolve(prob, P.alg; dt = P.dt, saveat=P.savedt, P.solver_opts...)
+    η = parameterseries(P, p=[4], transient=true)
+    α = parameterseries(P, p=[5], transient=true)
+    sol = sol .+ η.*randn(size(sol))
+    sol = (sol.*exp.(α.*(sol.-mean(sol, dims=2))))'
+end
+
+seeIfItSticksSim = Process(
+    process = seeIfItSticks,
+    X0 = [-3.0, 2.0, 20.0],
+    parameter_profile = (ramp, ramp, ramp, constant, constant),
+    parameter_profile_parameters = ((42.0, 42.0, 0.0, 500.0), (11.0, 11.0, 0.0, 500.0), (28.0, 28.0, 0.0, 500.0), 2.0, 0.01),
+    transient_t0 = -100.0,
+    t0 = 0.0,
+    dt = 0.001,
+    savedt = 0.025,
+    tmax = 500.0,
+    alg = RK4(),
+    solver_opts = Dict(:adaptive => false))
+export seeIfItSticksSim
--- test/WallpaperTest.jl
+++ test/WallpaperTest.jl
@@ -1,6 +1,29 @@
 using NonstationaryProcesses
 using Plots
 
+function wallPlot(S, vars)
+        # Square ones
+        if typeof(vars[2]) <: String
+                vars, name = vars
+        else
+                name = getprocess(S)
+        end
+        mkpath(joinpath(dir, "Square"))
+        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(900, 900), aspect_ratio=:equal, margin=15Plots.mm, dpi=1000, linewidth=1.0);
+        savefig(p, joinpath(dir, "Square", string(name)*".png"))
+
+        # 16:9
+        mkpath(joinpath(dir, "WallpaperSquare"))
+        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), aspect_ratio=:equal, margin=10Plots.mm, dpi=1000, linewidth=1.5);
+        savefig(p, joinpath(dir, "WallpaperSquare", string(name)*".png"))
+
+        # 16:9 fill
+        mkpath(joinpath(dir, "WallpaperFill"))
+        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), left_margin=40Plots.mm, right_margin=40Plots.mm, dpi=1000, linewidth=1.5);
+        savefig(p, joinpath(dir, "WallpaperFill", string(name)*".png"))
+end
+
+
 colorgradient = :turbo
 dir = "../ProcessImages/"
 mkpath(dir)
@@ -12,21 +35,13 @@ mkpath(dir)
         diffusionlessLorenzArt()                => 1:3,
         piecewiseLinearHyperchaosArt()          => [1, 3],
         simplifiedLorenz4DArt()                 => [3, 2, 4],
+        chensSystemArt()                        => [1, 2, 3],
+        chensSystemArt(parameters=(46.0, 11.0, 29.0), tmax=500.0)
+                                                => ([1, 2, 3], "chensSystem46"),
 )
 
+
+
 for (S, vars) ∈ 𝒮
-        # Square ones
-        mkpath(joinpath(dir, "Square"))
-        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(900, 900), aspect_ratio=:equal, margin=15Plots.mm, dpi=1000, linewidth=1.0);
-        savefig(p, joinpath(dir, "Square", string(getprocess(S))*".png"))
-
-        # 16:9
-        mkpath(joinpath(dir, "WallpaperSquare"))
-        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), aspect_ratio=:equal, margin=10Plots.mm, dpi=1000, linewidth=1.5);
-        savefig(p, joinpath(dir, "WallpaperSquare", string(getprocess(S))*".png"))
-
-        # 16:9 fill
-        mkpath(joinpath(dir, "WallpaperFill"))
-        p = plot(S; vars, colormethod=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), left_margin=40Plots.mm, right_margin=40Plots.mm, dpi=1000, linewidth=1.5);
-        savefig(p, joinpath(dir, "WallpaperFill", string(getprocess(S))*".png"))
+        wallPlot(S, vars)
 end
