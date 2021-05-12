e3378accd106cdd29e42b5e6521c0ecac96a8a1a
BrendanJHarris <brendanjohnharris@gmail.com>
2021-05-12 11:26:23 +1000

    Add custom high dim system
    Former-commit-id: 390f3aed5524aaa14597152cbbe139354f8885d9

--- test/animationtest.jl
+++ test/animationtest.jl
@@ -3,7 +3,6 @@ p = 0.0:0.01:3.0
 anim = @animate for ip ∈ p
     println(ip)
     S = simulate(waveDrivenHarmonicSim(X0 = [0.1, 0.0], parameter_profile_parameters = ((π,), (ip*π,), (π,), (π,))))
-    #println(DN_HistogramMode_10(Array(timeseries(S, 1))))
     plot(S, downsample=3, size=(1000, 1000), title="Ω = $ip", border=:none, grid=:none)
 end every 1
 gif(anim, fps = 20)
