2ca92c2ffa51a8eb7fe434a0edbc76d8f4e31c22
brendanjohnharris <brendanjohnharris@gmail.com>
2021-06-18 16:13:07 +1000

    Plot and animate phase-corrupted systems
    Former-commit-id: 3e84291ea13b488be535337636b6bc8b21bd6f68

---
+++ test/corruptedThomasAnimation.jl
@@ -0,0 +1,20 @@
+using NonstationaryProcesses
+using Plots
+
+
+tom = thomasCyclicallySymmetricSim(
+    X0 = [0.1, 0.0, 0.0],
+    parameter_profile = constantParameter,
+    parameter_profile_parameters = (0.18,),
+    transient_t0 = -100.0,
+    t0 = 0.0,
+    dt = 0.001,
+    savedt = 0.01,
+    tmax = 2000.0,
+    alg = AutoVern7(Rodas5()),
+    solver_opts = Dict(:adaptive => true, :reltol => 1e-12))
+
+𝔱𝔬𝔪 = corruptphase(tom, ramp, (0.0, 1.0, 0.0, 2000.0))()
+
+a = animatespectrum(𝔱𝔬𝔪, downsample=100, trail=5000)
+gif(a, fps=24)
