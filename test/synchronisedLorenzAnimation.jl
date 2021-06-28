8752d9ff0278b81646203fbaadd44faba1f74571
brendanjohnharris <brendanjohnharris@gmail.com>
2021-06-28 14:40:00 +1000

    Plain windowed fourier transforms (no fancy stft)
    Former-commit-id: d8b8c3a9f638ec2e52ca188858a1ad35c2c43eb6

---
+++ test/synchronisedLorenzAnimation.jl
@@ -0,0 +1,19 @@
+using NonstationaryProcesses
+using Plots
+
+lorenz = lorenzSim(
+    X0 = [0.0, -0.01, 9.0],
+    parameter_profile = (constantParameter, constantParameter, constantParameter),
+    parameter_profile_parameters = (10.0, 28.0, 8/3), # Sprott's recomendation
+    transient_t0 = -100.0,
+    t0 = 0.0,
+    dt = 0.001,
+    savedt = 0.01,
+    tmax = 1000.0,
+    alg = AutoVern7(Rodas5()),
+    solver_opts = Dict(:adaptive => true, :reltol => 1e-12))
+
+𝔩𝔬𝔯𝔢𝔫𝔷 = synchronisephase(lorenz, rampInterval, (0.0, 500.0, 250.0, 1000.0))()
+
+a = animatespectrum(𝔩𝔬𝔯𝔢𝔫𝔷, downsample=100, trail=5000, colorgradient=cgrad([:black, :crimson]), phasogram=true, nperseg=1000, dpi=100)
+gif(a, "./corruptlorenzsym.gif", fps=24)
