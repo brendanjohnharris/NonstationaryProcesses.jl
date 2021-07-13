10566c4c7a2fbfb37aa1579f7ce15bfdf37746e4
brendanjohnharris <brendanjohnharris@gmail.com>
2021-07-13 12:01:20 +1000

    Another plot: low pass filtering
    Former-commit-id: 787562482caab5b32683dfac4db1ad3b5e2503e3

--- test/lowpassLorenzAnimation.jl
+++ test/lowpassLorenzAnimation.jl
@@ -1,19 +1,20 @@
 using NonstationaryProcesses
 using Plots
 
-lorenz = lorenzSim(
+savedt = 0.01
+S = lorenzSim(
     X0 = [0.0, -0.01, 9.0],
     parameter_profile = (constantParameter, constantParameter, constantParameter),
     parameter_profile_parameters = (10.0, 28.0, 8/3), # Sprott's recomendation
     transient_t0 = -100.0,
     t0 = 0.0,
     dt = 0.001,
-    savedt = 0.01,
-    tmax = 8000.0,
+    savedt = savedt,
+    tmax = 1000.0,
     alg = AutoVern7(Rodas5()),
     solver_opts = Dict(:adaptive => true, :reltol => 1e-12))
 
-𝔩𝔬𝔯𝔢𝔫𝔷 = corruptphase(lorenz, rampInterval, (0.0, 0.2, 1000.0, 8000.0), nwindows = 160)()
+P = NonstationaryProcesses.lowpass(S, rampInterval, (1/(2*savedt), 0.0, 200.0, 1000.0), nwindows = 100)()
 
-a = animatespectrum(𝔩𝔬𝔯𝔢𝔫𝔷, downsample=500, trail=5000, colorgradient=cgrad([:black, :crimson]), phasogram=true, nwindows=160, dpi=100)
-gif(a, "./thresholdcorruptlorenzsym.mp4", fps=48)
+a = animatespectrum(P, downsample=50, trail=5000, colorgradient=cgrad([:black, :crimson]), phasogram=true, nwindows=100, dpi=100)
+gif(a, "./lowpasslorenzsym.mp4", fps=48)
