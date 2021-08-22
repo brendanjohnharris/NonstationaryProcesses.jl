1e964c160229a389cf3093ab6d4c8a6dcb265cdc
brendanjohnharris <brendanjohnharris@gmail.com>
2021-08-22 16:57:12 +1000

    Plot Lyapunov responses

--- src/Plotting/SystemCharacterisation.jl
+++ src/Plotting/SystemCharacterisation.jl
@@ -1,21 +1,29 @@
 function labeldims(n)
     if n == 1
-        ("t", "x")
+        ("\$t\$", "\$x\$")
     elseif n == 2
-        ("x", "y")
+        ("\$x\$", "\$y\$")
     else
-        ("x", "y", "z")
+        ("\$x\$", "\$y\$", "\$z\$")
     end
 end
 
-function stationarycharacteristics(P::Process; n = length(getX0(P)), labels=labeldims(n), xlabel=labels[1], ylabel=labels[2], zlabel=labels[2])
-    p1 = marginaltrajectory(P; xlabel, ylabel, zlabel, size=(400, 350), mainalpha=0.35)
+function stationarycharacteristics(P::Process; n = length(getX0(P)), labels=labeldims(n), xlabel=labels[1], ylabel=labels[2], zlabel=(length(labels) == 3 ? labels[3] : nothing), size=(400, 350))
+    p1 = marginaltrajectory(P; xlabel, ylabel, zlabel, size, mainalpha=0.35)
     display(p1)
     AMI = selfmutualinfo(P)
-    p2 = plot((AMI isa AbstractVector ? AMI : AMI[:, end:-1:1]), legend=true,     foreground_color_legend = nothing,
-    background_color_legend = nothing, legendtitle=nothing, size=(400, 350),
+    p2 = plot((AMI isa AbstractVector ? AMI : AMI[:, end:-1:1]); legend=true,     foreground_color_legend = nothing,
+    background_color_legend = nothing, legendtitle=nothing, size,
     xticks=[0, optimize_ticks(extrema(dims(AMI, 1).val)...; k_min = 3, k_max = 5)[1]...],
     yticks=optimize_ticks(extrema(AMI)...; k_min = 3, k_max = 6)[1],
-    xlim=(0, Inf))
+    xlim=(0, Inf), xlabel="\$\\tau\$")
+    display(p2)
+    return nothing
 end
 export stationarycharacteristics
+
+function plotlyapunovresponse(P::Process, p, prange; size=(400, 350), N=1000, k=length(getX0(P)), kwargs...)
+    λs = lyapunovresponse(P, p, prange, N, k)
+    plot(prange, λs; size, xlabel="\$\\theta\$", ylabel="\$\\lambda\$", kwargs...)
+end
+export plotlyapunovresponse
