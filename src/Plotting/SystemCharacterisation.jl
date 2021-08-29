5e5051aa5c5ef213fdb0b83bcc254398c8304be8
brendanjohnharris <brendanjohnharris@gmail.com>
2021-08-29 11:50:42 +1000

    Add Ikeda

--- src/Plotting/SystemCharacterisation.jl
+++ src/Plotting/SystemCharacterisation.jl
@@ -12,7 +12,7 @@ function stationarycharacteristics(P::Process; n = length(getX0(P)), labels=labe
     p1 = marginaltrajectory(P; xlabel, ylabel, zlabel, size, mainalpha=0.35)
     display(p1)
     AMI = selfmutualinfo(P)
-    p2 = plot((AMI isa AbstractVector ? AMI : AMI[:, end:-1:1]); legend=true,     foreground_color_legend = nothing,
+    p2 = plot(Array((AMI isa AbstractVector ? AMI : AMI[:, end:-1:1])); legend=true,     foreground_color_legend = nothing,
     background_color_legend = nothing, legendtitle=nothing, size,
     xticks=[0, optimize_ticks(extrema(dims(AMI, 1).val)...; k_min = 3, k_max = 5)[1]...],
     yticks=optimize_ticks(extrema(AMI)...; k_min = 3, k_max = 6)[1],
