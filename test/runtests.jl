c79a8b4e60fc546cb83a410703c092e300ec159d
brendanjohnharris <brendanjohnharris@gmail.com>
2023-05-23 14:56:55 +1000

    Restructure for the aesthetic

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using NonstationaryProcesses
 using Test
 
 @testset "Simulations" begin
-    @test_nowarn include("./ARTests.jl")
-    @test_nowarn include("./HenonTest.jl")
-    @test_nowarn include("./lyapunovTest.jl")
-    @test_nowarn include("./testGaussianBimodal.jl")
+    @test_nowarn include("./datasets/honours/ARTests.jl")
+    @test_nowarn include("./datasets/honours/HenonTest.jl")
+    @test_nowarn include("./datasets/honours/lyapunovTest.jl")
+    @test_nowarn include("./datasets/honours/testGaussianBimodal.jl")
 end
