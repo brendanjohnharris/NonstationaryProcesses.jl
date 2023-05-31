06f80f1e63ff295614d41e7ab0249fb80e748602
brendanjohnharris <brendanjohnharris@gmail.com>
2023-05-31 10:35:12 +1000

    Update phase-corrupted lorenz
    Former-commit-id: c5ecadf957bd1106f59b784226c40678453d7a1e [formerly 8fb011525a9ef9a39747e67da3056eab238df249 [formerly 7501cf117f486c528e2be771de421e368f9353bf] [formerly 217dcaa75582c05d20c27ca0c2273b615084123b] [formerly 217dcaa75582c05d20c27ca0c2273b615084123b [formerly f840c596f7f47246cac14525ca0b1e894abd0e24]]]
    Former-commit-id: 8fb011525a9ef9a39747e67da3056eab238df249 [formerly 7501cf117f486c528e2be771de421e368f9353bf] [formerly 217dcaa75582c05d20c27ca0c2273b615084123b]
    Former-commit-id: 5454f3ddbf2295d0d72ea4f9522c7fde00a6764f
    Former-commit-id: dc8d378fe7e352e9b771e3c1669972475961eb12 [formerly b74ce23fea7d7abda9f095c2a1ca03d80e9a0aa1]
    Former-commit-id: dbf928c2f25997c41724be7c3ba48c9b61341bcd

--- test/datasets/stationary/PhaseCorruptedLorenz/phaserandomization_comparison.jl
+++ test/datasets/stationary/PhaseCorruptedLorenz/phaserandomization_comparison.jl
@@ -1,4 +1,5 @@
-using WGLMakie
+using CairoMakie
+import TimeseriesTools.TimeSeries
 using TimeseriesTools
 using DifferentialEquations
 using TimeseriesSurrogates
@@ -6,10 +7,9 @@ using NonstationaryProcesses
 using NonstationaryProcesses.DifferentialEquationsExt
 using Foresight
 using FFTW
-using JSServe
 
 transient = 20000
-linewidth = 0.1
+linewidth = 0.05
 
 lorenz = lorenzSim(
     X0 = [0.0, -0.01, 9.0],
@@ -18,8 +18,8 @@ lorenz = lorenzSim(
     transient_t0 = -100.0,
     t0 = 0.0,
     dt = 0.001,
-    savedt = 0.01,
-    tmax = 800.0,
+    savedt = 0.025,
+    tmax = 1500.0,
     alg = AutoVern7(Rodas5()),
     solver_opts = Dict(:adaptive => true, :reltol => 1e-12))
 
@@ -30,60 +30,54 @@ fs = rfftfreq(size(sx, 1))
 _x = sx[transient:end-transient, :]
 x = _x[:, 1]
 
-alg = :relative
-α⃗ = 0:0.1:0.3
-α = 0.1
+algs = [PartialRandomization, RelativePartialRandomization, SpectralPartialRandomization]
+α⃗ = [0, 0.1, 1]
 
-f = Figure(; resolution=(1980, 800))
+for α in α⃗
+for alg in algs
+@info "α = $α, alg = $alg"
+f = Figure(; resolution=(800, 1200))
 
-ss = cat(TimeseriesSurrogates.surrogate.(eachcol(sx), [PartialRandomization(α, alg)])..., dims=Var)
+ss = cat(TimeseriesSurrogates.surrogate.(eachcol(sx), [alg(α)])..., dims=Var)
 _s = ss[transient:end-transient, :]
 s = _s[:, 1]
-ax = [Axis(f[1, 1]), Axis(f[2, 1]; title="Power spectrum")]
+ax = [Axis(f[1, 1:2]), Axis(f[2, 1:2]; title="Power spectrum")]
 surroplot!(ax, x, s)
 axislegend(ax[2])
-xlims!(ax[1], (0, 2000))
-xlims!(ax[2], (0, 0.15))
-ax[1].title="Time series for α = $α, alg = :$alg"
+xlims!(ax[1], (0, 1000))
+xlims!(ax[2], (0, 0.3))
+ax[1].title="Time series for α = $α, alg = $alg"
+ax[1].titlesize = 25
 
 # Trajectory
-ax = Axis3(f[1, 2]; title="Original trajectory")
-trajectory!(ax, collect.(eachcol(_x))...; colormap=:turbo, linewidth)
-shadows!(ax, collect.(eachcol(_x))...; color=(:black, 0.5), linewidth)
+ax = Axis3(f[3, 1]; title="Original trajectory")
+trajectory!(ax, collect.(eachcol(_x))...; colormap=:turbo, linewidth=linewidth*2)
+ax.xlabelvisible = ax.ylabelvisible = ax.zlabelvisible = ax.xticksvisible = ax.yticksvisible = ax.zticksvisible = ax.xticklabelsvisible = ax.yticklabelsvisible = ax.zticklabelsvisible = false
+ax.azimuth = ax.azimuth[] + 0.25
+ax.elevation = ax.elevation[] + 0.25
+shadows!(ax, collect.(eachcol(_x))...; color=(:slategray, 0.5), linewidth)
 
-ax = Axis3(f[2, 2]; title="Surrogate trajectory")
-trajectory!(ax, collect.(eachcol(_s))...; colormap=:turbo, linewidth)
-shadows!(ax, collect.(eachcol(_s))...; color=(:black, 0.5), linewidth)
+ax = Axis3(f[3, 2]; title="Surrogate trajectory")
+ax.azimuth = ax.azimuth[] + 0.25
+ax.elevation = ax.elevation[] + 0.25
+trajectory!(ax, collect.(eachcol(_s))...; colormap=:turbo, linewidth=linewidth*2)
+ax.xlabelvisible = ax.ylabelvisible = ax.zlabelvisible = ax.xticksvisible = ax.yticksvisible = ax.zticksvisible = ax.xticklabelsvisible = ax.yticklabelsvisible = ax.zticklabelsvisible = false
+shadows!(ax, collect.(eachcol(_s))...; color=(:slategray, 0.5), linewidth)
 
 # Phasogram
-# ax = Foresight.PolarAxis(f[1, 3]; title="Original phasogram")
-ax = Axis(f[1, 3]; title="Original phasogram", xlabel="Frequency", ylabel="Phase")
-lines!(ax, fs, ϕ)
+ax = Foresight.PolarAxis(f[4, 1]; title="Original phasogram", rticklabelcolor = (:black, 0.0))
+# ax = Axis(f[4, 1]; title="Original phasogram", xlabel="Frequency", ylabel="Phase")
+scatter!(ax, fs, ϕ, markersize=1)
 
 ϕ′ = ss[:, 1] |> rfft .|> angle
-# ax = Foresight.PolarAxis(f[2, 3]; title="Surrogate phasogram")
-ax = Axis(f[2, 3]; title="Surrogate phasogram", xlabel="Frequency", ylabel="Phase")
-lines!(ax, fs, ϕ′)
+ax = Foresight.PolarAxis(f[4, 2]; title="Surrogate phasogram", rticklabelcolor = (:black, 0.0))
+# ax = Axis(f[4, 2]; title="Surrogate phasogram", xlabel="Frequency", ylabel="Phase")
+scatter!(ax, fs, ϕ′, markersize=1)
 
-colsize!(f.layout, 1, Relative(0.5))
-colgap!(f.layout, 1, Relative(0.07))
-colgap!(f.layout, 2, Relative(0.0))
+rowsize!(f.layout, 1, Relative(0.2))
+rowsize!(f.layout, 2, Relative(0.2))
+rowsize!(f.layout, 3, Relative(0.3))
+save(joinpath(@__DIR__, "lorenz_surrogates_$(α)_$alg.png"), f)
 
-# save(joinpath(@__DIR__, "lorenz_surrogates.pdf"), f)
-
-
-
-# open("index.html", "w") do io
-#     println(io, """
-#     <html>
-#         <head>
-#         </head>
-#         <body>
-#     """)
-#     JSServe.Page(exportable=true, offline=true)
-#     show(io, MIME"text/html"(), f)
-#     println(io, """
-#         </body>
-#     </html>
-#     """)
-# end
+end
+end
