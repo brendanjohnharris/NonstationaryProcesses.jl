34bb2e1e6d6d5c620175068cf71950c39585715a
brendanjohnharris <brendanjohnharris@gmail.com>
2023-11-24 09:44:56 +1100

    Add dequanLi
    Former-commit-id: 50eeb472cc3255d84028a5bec22fb7a9e40eff49 [formerly bebf11c89f85fee8d6e46138a0233ebc07653aed [formerly 8e84ee5b653b6acd087143996b0e0248c2c21137] [formerly 8635dcee7feb97a4bcb2648138e7defeffe2e728] [formerly 8635dcee7feb97a4bcb2648138e7defeffe2e728 [formerly 1f216d998b4d9693cc4e7751df2d4c78cdeb1469]]]
    Former-commit-id: bebf11c89f85fee8d6e46138a0233ebc07653aed [formerly 8e84ee5b653b6acd087143996b0e0248c2c21137] [formerly 8635dcee7feb97a4bcb2648138e7defeffe2e728]
    Former-commit-id: 73c1ba08861f5b5d944acadbf87140bce9713082
    Former-commit-id: ca1b8efd9e2de3dfab1a11e39402c9a5cc457ead [formerly 0dae8a302c51a8c28c7678ab60830f888a38f337]
    Former-commit-id: 60cb57db2b65719df4a682afb1323aa271695032

---
+++ test/Wallpapers/WallpaperTest.jl
@@ -0,0 +1,73 @@
+using Plots
+using StatsPlots
+using DifferentialEquations
+using NonstationaryProcesses
+import NonstationaryProcesses.DifferentialEquationsExt as DE
+
+function wallPlot(S, vars; kwargs...)
+    # Square ones
+    if vars[2] isa String
+        vars, name = vars
+    else
+        name = getprocess(S)
+    end
+    mkpath(joinpath(dir, "Square"))
+    display(S)
+    display(vars)
+    p = plot(S; vars, colormode=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(900, 900), aspect_ratio=:equal, margin=15Plots.mm, dpi=1000, linewidth=1.0, kwargs...)
+    savefig(p, joinpath(dir, "Square", string(name) * ".png"))
+
+    # 16:9
+    mkpath(joinpath(dir, "WallpaperSquare"))
+    p = plot(S; vars, colormode=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), aspect_ratio=:equal, margin=10Plots.mm, dpi=1000, linewidth=1.5, kwargs...)
+    savefig(p, joinpath(dir, "WallpaperSquare", string(name) * ".png"))
+
+    # 16:9 fill
+    mkpath(joinpath(dir, "WallpaperFill"))
+    p = plot(S; vars, colormode=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(1600, 900), left_margin=40Plots.mm, right_margin=40Plots.mm, dpi=1000, linewidth=1.5, kwargs...)
+    savefig(p, joinpath(dir, "WallpaperFill", string(name) * ".png"))
+end
+
+
+colorgradient = :turbo
+dir = joinpath(splitpath(Base.active_project())[1:end-1]..., "Wallpapers")
+mkpath(dir)
+
+# Dict of system => variables to plot
+𝒮 = Dict(
+    # DE.simplestChaoticFlowVis() => 2:3, # I think these need to be updated for in-place
+    # DE.thomasCyclicallySymmetricVis() => 1:2,
+    # doubleScrollVis()                       => 1:3,
+    # diffusionlessLorenzVis()                => 1:3,
+    # piecewiseLinearHyperchaosVis()          => [1, 3],
+    # simplifiedLorenz4DVis()                 => [3, 2, 4],
+    # chensSystemVis()                        => [1, 2, 3],
+    # chensSystemVis(parameters=(46.0, 11.0, 29.0), tmax=500.0)
+    #                                         => ([1, 2, 3], "chensSystem46"),
+    # cartesianDoublePendulumSim(profiles=(constant, constant, constant, constant),
+    #                            parameters=(0.5, 1.0, 1.0, 1.0),
+    #                            X0=[π/4, π, 0.0, 0.0],
+    #                            tmax=2000.0,
+    #                            savedt= 0.01)
+    #                                         => [3, 4],
+    # DE.lorenzVis() => 1:3,
+    DE.dequanLiVis() => 1:3,
+)
+
+for (S, vars) ∈ 𝒮
+    wallPlot(S, vars; N=100000)
+end
+
+
+# dequanLi is tricky
+begin
+    S = DE.dequanLiVis()
+    vars = 1:3
+    kwargs = ()
+    S.dt = 0.001
+    S.savedt = 0.001
+    S.tmax = 10000.0
+    solver_opts = Dict(:adaptive => true, :reltol => 1e-13, :maxiters => 1e12)
+    p = plot(S; vars, colormode=:velocity, linecolor=colorgradient, background_color=:black, axis=nothing, framestyle=:none, size=(900, 900), aspect_ratio=:equal, margin=15Plots.mm, dpi=1000, linewidth=1.0, N=1000000, linealpha=0.025, kwargs...)
+    savefig(p, joinpath(dir, "Square", "DequanLiRepeats" * ".png"))
+end
